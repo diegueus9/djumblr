e37b1e265bd3db12103e15977b0965008a2e7ec3
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-05-29 02:54:17 -0500

    Working in a command for design models and test python-tumblr

--- management/commands/import_tumblr_blog.py
+++ management/commands/import_tumblr_blog.py
@@ -1,5 +1,44 @@
 # -*- coding: utf-8 -*-
+import getpass
+from copy import copy
+
+from tumblr import Api
+from dateutil import parser
+
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import get_model
+
+def translate_dict(post):
+    data = {}
+    for key, value in post.items():
+        key = key.replace('-', '_')
+        if key == 'id':
+            key = 'tumblr_id'
+        elif key in ['date', 'date_gmt']:
+            value = parser.parse(value)
+        elif key in ['tags', 'photos']:
+            key = None
+        if key is not None:
+            data[key] = value
+    return data
+
+def _(post_data):
+    photo_model = get_model('djumblr', 'PhotoPost')
+    video_model = get_model('djumblr', 'VideoPost')
+    text_model = get_model('djumblr', 'TextPost')
+    link_model = get_model('djumblr', 'LinkPost')
+    data = translate_dict(copy(post_data))
+    if data['type'] == 'photo':
+        post_instance = photo_model(**data)
+    elif data['type'] == 'video':
+        post_instance = video_model(**data)
+    elif data['type'] == 'regular':
+        post_instance = text_model(**data)
+    elif data['type'] == 'link':
+        post_instance = link_model(**data)
+    else:
+        print post_data
+    post_instance.save()
 
 class Command(BaseCommand):
     help = 'Import posts from tumblr'
@@ -8,4 +47,20 @@ class Command(BaseCommand):
     can_import_settings = True
 
     def handle(self, *args, **options):
+        tumblr_model = get_model('djumblr', 'Tumblr')
+        
+        tumblr_name = 'diegueus9'
+        tumblr_email = 'diegueus9@gmail.com'
         print 'working...'
+        tumblr_api = Api(tumblr_name, tumblr_email, getpass.getpass('Your tumblr password:'))
+        
+        t, created = tumblr_model.objects.get_or_create(name=tumblr_name, email=tumblr_email)
+        tumblr_response = tumblr_api.read()
+        for post in tumblr_response:
+            _(post)
+            #try:
+                #_(post)
+            #except:
+                #print post
+                #break
+                
--- models.py
+++ models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+class Tumblr(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+class TagInTumblr(models.Model):
+    tag = models.ForeignKey(Tag)
+    tumblr = models.ForeignKey(Tumblr)
+
+class Post(models.Model):
+    tumblr_id = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    format = models.CharField(max_length=255)
+    
+    reblog_key = models.CharField(max_length=255)
+
+    feed_item = models.URLField(verify_exists=False, blank=True, null=True)
+    from_feed_id = models.CharField(max_length=255, blank=True, null=True)
+
+    mobile = models.IntegerField() # i think this is a boolean
+    bookmarklet = models.IntegerField() # i dont knoe what is this but i suspect is a boolean
+
+    unix_timestamp = models.IntegerField()
+    date = models.DateTimeField()
+    date_gmt = models.DateTimeField()
+
+    url = models.URLField(verify_exists=False)
+    slug = models.SlugField()
+    url_with_slug = models.URLField(verify_exists=False)
+
+class PhotoPost(Post):
+    photo_caption = models.TextField(blank=True, null=True)
+    photo_link_url = models.URLField(verify_exists=False)
+    #tags = models.ManyToManyField(Tag)
+    #photos = models.ManyToMany... # i dont know what is this
+    
+    height = models.IntegerField()
+    width = models.IntegerField()
+
+    photo_url_75 = models.URLField(verify_exists=False)
+    photo_url_100 = models.URLField(verify_exists=False)
+    photo_url_250 = models.URLField(verify_exists=False)
+    photo_url_400 = models.URLField(verify_exists=False)
+    photo_url_500 = models.URLField(verify_exists=False)
+    photo_url_1280 = models.URLField(verify_exists=False)
+    
+class VideoPost(Post):
+    video_caption = models.TextField(blank=True, null=True)
+    video_source = models.URLField(verify_exists=False)
+    video_player = models.TextField()
+    video_player_250 = models.TextField()
+    video_player_500 = models.TextField()
+
+class TextPost(Post):
+    regular_body = models.TextField()
+    regular_title = models.CharField(max_length=255, blank=True, null=True)
+
+class LinkPost(Post):
+    link_url = models.URLField(verify_exists=False)
+    link_description = models.TextField()
+    link_text = models.CharField(max_length=255, blank=True, null=True)
+
+
+
+    
+    
